dcf1c3dda657b00c56728bf1e38c614783c88609
m-dwyer <mark.andrew.dwyer@gmail.com>
2016-04-21 15:36:43 +1000

    Add NcentralUtil module used by remote N-central agents to push PSObjects remotely as CSV

---
+++ NcentralUtil.psm1
@@ -0,0 +1,193 @@
+﻿function Get-AgentPropertyValue
+{
+    [CmdletBinding()]
+    param([switch] $Force = $false, [string] $PropertyName, [string] $PropertyXPath)
+
+    try
+    {
+        $propertyValue = $null
+
+        if (-not $Force)
+        {
+            $keyProperties = Get-ItemProperty -ErrorAction SilentlyContinue "HKLM:\SOFTWARE\N-able Technologies"
+            if ($keyProperties -ne $null)
+            {
+                $propertyValue = $keyProperties."$propertyName"
+            }
+        }
+
+        if ($propertyValue -eq $null)
+        {
+            try
+            {
+                $appConfigFile = "${env:ProgramFiles(x86)}\N-able Technologies\Windows Agent\config\ApplianceConfig.xml"
+                $appConfigXml = New-Object System.Xml.XmlDocument
+                $fileStream = New-Object System.IO.Filestream $appConfigFile, 'Open', 'Read', 'ReadWrite'
+
+                $appConfigXml.Load($fileStream)
+                $propertyValue = $appConfigXml.SelectSingleNode("$PropertyXPath").Value
+            }
+            catch 
+            {
+                Write-Verbose "An error occurred reading ApplianceConfig.xml: $($_.Exception.Message)"
+            }
+            finally
+            {
+                $fileStream.Dispose()
+            }
+
+            Set-ItemProperty "HKLM:\SOFTWARE\N-able Technologies" -Name "$PropertyName" -Value $propertyValue
+        }
+    }
+    catch
+    {
+        $computerSystem = Get-WmiObject -Class Win32_ComputerSystem
+        $propertyValue = "$($computerSystem.Name)-$($computerSystem.Domain)"
+    }
+
+    return $propertyValue
+}
+
+Function Get-ApplianceID
+{
+    [CmdletBinding()]
+    param([switch] $Force = $false)
+
+    Get-AgentPropertyValue -Force:$Force -PropertyName "ApplianceID" -PropertyXPath "/ApplianceConfig/ApplianceID/text()"
+}
+
+Function Get-CustomerID
+{
+    [CmdletBinding()]
+    param([switch] $Force = $false)
+
+    Get-AgentPropertyValue -Force:$Force -PropertyName "CustomerID" -PropertyXPath "/ApplianceConfig/CustomerID/text()"
+}
+
+Function Get-RemoteFileName
+{
+    [CmdletBinding()]
+    param([string] $FileName)
+
+    $applianceId = Get-ApplianceID
+    return "$($applianceId)_$FileName"
+}
+
+Function Upload-File
+{
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$true)]
+    $LocalFile,
+    [Parameter(Mandatory=$true)]
+    [string] $URI,
+    [Parameter(Mandatory=$true)]
+    [string] $RemoteFile,
+    [switch] $NoValidate = $false,
+    [int] $RetryWait = 10,
+    [int] $RetryCount = 5
+    )
+
+    if ($NoValidate)
+    {
+        [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+    }
+
+    $uriObj = New-Object System.Uri "$URI/$RemoteFile"
+    $userInfo = $uriObj.UserInfo.Split(":")
+
+    Write-Verbose "Reading local file [$LocalFile] bytes.."
+    $fileContents = [System.IO.File]::ReadAllBytes($LocalFile)
+
+    $attemptCount = 0
+    $uploadSuccess = $false
+
+    while ($attemptCount -lt $RetryCount -and $uploadSuccess -ne $true)
+    {
+        try 
+        {
+            $request = $null
+
+            if ($uriObj.Scheme -eq 'https')
+            {
+                $request = [System.Net.HttpWebRequest]::Create($uriObj)
+                $request.Method = [System.Net.WebRequestMethods+Http]::Put
+                $auth = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$($userInfo[0]):$($userInfo[1])"));
+                $request.Headers["Authorization"] = "Basic " + $auth
+            }
+            elseif ($uriObj.Scheme -eq 'ftp')
+            {
+                $request = [System.Net.FtpWebRequest]::Create($uriObj)
+                $request.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+                $request.Credentials = New-Object System.Net.NetworkCredential($userInfo[0], $userInfo[1])
+                $request.UseBinary = $true
+                $request.UsePassive = $true
+                $request.EnableSsl = $true
+                $request.KeepAlive = $false
+            }
+
+            $request.ContentLength = $fileContents.Length
+            $requestStream = $request.GetRequestStream()
+
+            Write-Verbose "Writing local file [$localFile] to remote URI [$URI/$RemoteFile]"
+            $requestStream.Write($fileContents, 0, $fileContents.Length)
+            $requestStream.Close()
+            $response = $request.GetResponse()
+            Write-Verbose "Result: [$($response.StatusDescription -replace "`r`n", ' ')]"
+            $response.Close()
+
+            $uploadSuccess = $true
+        }
+        catch
+        {
+            if ($requestStream -ne $null) { $requestStream.Close() }
+            if ($response -ne $null) { $response.Close() }
+            $attemptCount++
+            $exceptionMessage = "Upload failed $attemptCount attempts: $($_.Exception.Message)"
+            if ($attemptCount -lt $RetryCount)
+            {
+                $exceptionMessage  += "`r`nTrying again after $RetryWait seconds.."
+            }
+            Write-Error $exceptionMessage
+
+            Start-Sleep -Seconds $RetryWait
+        }
+    }
+
+    Write-Verbose "$(if ($uploadSuccess) { "Finished" } else { "Failed" }) after $attemptCount attempts"
+}
+
+Function Upload-Object
+{
+    [CmdletBinding()]
+    param(
+    [Parameter(
+        Mandatory=$true,
+        Position=0,
+        ValueFromPipeline=$true)
+    ]
+    [PSObject[]]$InputObject,
+    [string] $OutputName,
+    [Parameter(Mandatory=$true)]
+    $URI
+    )
+        begin
+        {
+            $objects = @()
+            $csvTempFileName = [System.IO.Path]::GetTempFileName()
+        }
+
+        process
+        {
+            foreach ($obj in $InputObject)
+            {
+                $objects += $obj
+            }
+        }
+
+        end
+        {
+            $objects | Export-Csv -Path $csvTempFileName
+            Upload-File -Local $csvTempFileName -URI $URI -RemoteFile "$(Get-RemoteFileName -FileName $OutputName).csv" -NoValidate
+        }
+}
